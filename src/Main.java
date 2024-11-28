2cc161c9b0dca860a38162cd356c3dacad6cb7cc
eddieyadete <eddieyadete123@gmail.com>
2024-11-27 23:32:01 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         AutoTypeConversion obj = new AutoTypeConversion();
 //        obj.usingTypeConversion();
-        obj.autoPromoteTest();
+       // obj.autoPromoteTest();
+        obj.explicitTest();
     }
 
 }
